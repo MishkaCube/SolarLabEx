b619b31bf53bedd03aa5c7b1ecfed4021f274435
Mishka <91431939+MishkaCube@users.noreply.github.com>
2022-01-19 19:57:57 +0300

    Add files via upload

---
+++ Level 1/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        AddBirthday adds = new AddBirthday();
+
+        MainActivity chose = new MainActivity();
+    }
+
+}
